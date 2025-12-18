7ef7d4eb33396d18ec79ed1e08432887f364d7c1
Md Badrul Alam <developer.badrul@gmail.com>
2025-12-18 23:23:40 +0600

    successfully add new post

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,31 +1,61 @@
-import React from 'react';
+import { useState } from 'react';
 
-const AddPost = () => {
+const AddPost = ({ onAddPost }) => {
+    const [form, setForm] = useState({
+        title: "",
+        body: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setForm(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    }
+
+    const isValid = form.title.trim() && form.body.trim();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!form.title.trim() || !form.body.trim()) return;
+
+        onAddPost(form);
+
+        setForm({
+            title: "",
+            body: "",
+        })
+    }
+
     return (
         <div>
             <h2>Add new post</h2>
 
-            <form >
+            <form onSubmit={handleSubmit}>
                 <p>
                     <input
+                        name='title'
                         type="text"
                         placeholder="Post title"
-                        // value={title}
-                        // onChange={(e) => setTitle(e.target.value)}
+                        value={form.title}
+                        onChange={handleChange}
                     />
                 </p>
 
                 <p>
                     <input
+                        name='body'
                         type="text"
                         placeholder="Post body"
-                        // value={body}
-                        // onChange={(e) => setBody(e.target.value)}
+                        value={form.body}
+                        onChange={handleChange}
                     />
                 </p>
 
                 <div>
-                    <input type="submit" />
+                    <input title='Submit' type="submit" disabled={!isValid} />
                 </div>
             </form>
         </div>
